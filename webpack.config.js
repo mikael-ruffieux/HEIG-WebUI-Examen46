9e28256bc65f46b92f34a3990ce73a97146bead9
Mikael Ruffieux <mikael@redox-prod.ch>
2021-04-11 11:33:03 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  mode: 'production',
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: false,
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test:/\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {url: false}
+          }
+        ]
+      },
+      {
+        test: /\.(handlebars|hbs)$/,
+        use: [{
+            loader: 'handlebars-loader',
+            options: {
+                helperDirs: path.resolve('./src/templates/helpers')
+            }
+        }]
+      } 
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules')
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new MiniCssExtractPlugin(),
+  ]
+};
